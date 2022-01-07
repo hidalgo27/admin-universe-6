660edd1b79f33db1c9a900cd56c99d20ef663ce7
Hidalgo Ponce <hidalgoponce@Hidalgos-MacBook-Pro.local>
2022-01-07 14:53:20 -0500

    Pdf update

--- resources/views/admin/pdf/package.blade.php
+++ resources/views/admin/pdf/package.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>Document</title>
+    <title>{{$user->name}}</title>
 {{--    <link rel="preconnect" href="https://fonts.googleapis.com">--}}
 {{--    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>--}}
 
